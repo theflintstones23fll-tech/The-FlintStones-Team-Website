6a18f657188e4b4521d590967a6d3fe72a1e7f8d
theflintstones23fll-tech <theflintstones23fll@gmail.com>
2026-01-11 16:46:58 +0300

    Updated Score Calculation Sheet

--- app/main.py
+++ app/main.py
@@ -9,5 +9,11 @@ templates = Jinja2Templates(directory="templates")
 @app.get("/",response_class = HTMLResponse)
 def home(request:Request):
     return templates.TemplateResponse(
+        "index.html", {"request": request}
+    )
+
+@app.get("/score",response_class = HTMLResponse)
+def home(request:Request):
+    return templates.TemplateResponse(
         "Score_Calculation.html", {"request": request}
     )
